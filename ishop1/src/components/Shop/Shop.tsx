022b6b0aadb4b8add99a3f8eaaf19f89d3b22f76
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2025-11-24 19:34:46 +0300

    add keys to mapped product rows & nest table rows in tbody

--- ishop1/src/components/Shop/Shop.tsx
+++ ishop1/src/components/Shop/Shop.tsx
@@ -1,19 +1,22 @@
 import './Shop.scss';
 import { Product, ProductRow } from "../Product/Product";
 
-export default function Shop({ productList }: { productList: Array<Product> }) {
+export default function Shop({ productList }: { productList: Array<Product & { id: string }> }) {
     return <table className='Shop'>
-        <tr>
-            <th>Название</th>
-            <th>Цена</th>
-            <th>Картинка</th>
-            <th>Количество</th>
-        </tr>
-        {productList.map(product => <ProductRow
-            name={product.name}
-            price={product.price}
-            imgUrl={product.imgUrl}
-            quantity={product.quantity}
-        />)}
+        <tbody>
+            <tr>
+                <th>Название</th>
+                <th>Цена</th>
+                <th>Картинка</th>
+                <th>Количество</th>
+            </tr>
+            {productList.map(product => <ProductRow
+                key={product.id}
+                name={product.name}
+                price={product.price}
+                imgUrl={product.imgUrl}
+                quantity={product.quantity}
+            />)}
+        </tbody>
     </table>
 }
