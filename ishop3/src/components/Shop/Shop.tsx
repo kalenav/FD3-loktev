1c6e3ded6bc5befa4a5ce018b7296d57f92006f1
Kanstantsin Loktsieu <konstantin.loktev@innowise.com>
2026-03-17 12:44:51 +0100

    shop component selection, form filling & blocking logic

--- ishop3/src/components/Shop/Shop.tsx
+++ ishop3/src/components/Shop/Shop.tsx
@@ -1,43 +1,98 @@
-import { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { Product as ProductType } from "../../model/product.interface";
-import Product from "../Product/Product";
+import Card from "../Card/Card";
+import Form from "../Form/Form";
+import { NonEmptyStringValidator } from "../Form/validators/empty-string.validator";
+import { NumberStringValidator } from "../Form/validators/number-string.validator";
 import './Shop.scss';
+import ProductList from "./subcomponents/ProductList/ProductList";
 
 export default function Shop({ products }: { products: Array<ProductType> }) {
-  const [selected, setSelected] = useState<number | null>(null);
   const [productList, setProductList] = useState(products.slice());
+  const [selectedProduct, setSelectedProduct] = useState<ProductType | null>(null);
+  const [formActive, setFormActive] = useState(false);
+  const [formDirty, setFormDirty] = useState(false);
+
+  const selectProduct = useCallback((product: ProductType) => {
+    if (formActive && formDirty) {
+      return;
+    }
+    setSelectedProduct(product);
+    setFormActive(false);
+  }, [formActive, formDirty]);
+
+  const startProductEdit = useCallback((product: ProductType) => {
+    selectProduct(product);
+    setFormActive(true);
+    setFormDirty(false);
+  }, [selectProduct]);
+  const concludeProductEdit = useCallback((editedProduct: ProductType) => {
+    setFormActive(false);
+    console.log(editedProduct);
+  }, []);
+
+  const deleteProduct = useCallback((id: number) => {
+    // eslint-disable-next-line no-restricted-globals
+    if (!confirm('Are you sure?')) {
+      return;
+    }
+    const newProductList = productList.slice();
+    newProductList.splice(newProductList.findIndex(p => p.id === id), 1);
+    setProductList(newProductList);
+  }, [productList]);
 
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Price</th>
-          <th>URL</th>
-          <th>Quantity</th>
-          <th>Control</th>
-        </tr>
-      </thead>
-      <tbody>
-        {productList.map((product, index) => (
-          <Product
-            key={product.id}
-            product={product}
-            selected={selected === product.id}
-            onSelect={() => setSelected(product.id)}
-            onDelete={() => {
-              // eslint-disable-next-line no-restricted-globals
-              if (!confirm('Are you sure?')) {
-                return;
+    <div className="shop">
+      <ProductList
+        products={productList}
+        onSelect={selectProduct}
+        onEdit={startProductEdit}
+        onDelete={deleteProduct}
+        buttonsDisabled={formActive && formDirty}
+        forcedSelectedProductId={(formActive && formDirty) ? selectedProduct?.id : undefined}
+      />
+      {formActive && (
+        <React.Fragment key={selectedProduct?.id ?? 0}>
+          <h2>Edit product</h2>
+          <Form
+            fields={[
+              {
+                name: 'name',
+                label: 'Name',
+                defaultValue: selectedProduct?.name || '',
+                validators: [NonEmptyStringValidator]
+              },
+              {
+                name: 'price',
+                label: 'Price',
+                defaultValue: `${selectedProduct?.price || ''}`,
+                validators: [NonEmptyStringValidator, NumberStringValidator]
+              },
+              {
+                name: 'url',
+                label: 'URL',
+                defaultValue: selectedProduct?.url || '',
+                validators: [NonEmptyStringValidator]
+              },
+              {
+                name: 'quantity',
+                label: 'Quantity',
+                defaultValue: `${selectedProduct?.quantity || ''}`,
+                validators: [NonEmptyStringValidator, NumberStringValidator]
               }
-              (product.id === selected) && setSelected(null);
-              const newProductList = productList.slice();
-              newProductList.splice(index, 1);
-              setProductList(newProductList);
-            }}
+            ]}
+            onDirty={() => setFormDirty(true)}
+            onSubmit={formValues => concludeProductEdit({
+              id: selectedProduct!.id,
+              ...formValues
+            } as ProductType)}
+            onCancel={() => { setFormActive(false); setFormDirty(false); }}
           />
-        ))}
-      </tbody>
-    </table>
+        </React.Fragment>
+      )}
+      {!!selectedProduct && !formActive && (
+        <Card name={selectedProduct.name} price={selectedProduct.price} />
+      )}
+    </div>
   )
 }
