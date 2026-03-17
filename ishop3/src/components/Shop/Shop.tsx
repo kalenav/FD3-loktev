79a6467f16aa1ac72f75780ac87435f6c54a3b06
Kanstantsin Loktsieu <konstantin.loktev@innowise.com>
2026-03-17 13:35:38 +0100

    shop component refactoring

--- ishop3/src/components/Shop/Shop.tsx
+++ ishop3/src/components/Shop/Shop.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { Product as ProductType } from "../../model/product.interface";
 import Card from "../Card/Card";
 import './Shop.scss';
@@ -7,46 +7,19 @@ import ProductList from "./subcomponents/ProductList/ProductList";
 
 export default function Shop({ products }: { products: Array<ProductType> }) {
   const [productList, setProductList] = useState(products.slice());
-  const [selectedProduct, setSelectedProduct] = useState<ProductType | null>(null);
   const [formActive, setFormActive] = useState(false);
   const [formDirty, setFormDirty] = useState(false);
+  const [selectedProductId, setSelectedProductId] = useState<number | null>(null);
+  const selectedProduct = useMemo(() => {
+    return productList.find(p => p.id === selectedProductId) || null;
+  }, [productList, selectedProductId]);
 
-  const getProductById = useCallback((id: number) => {
-    return productList.find(p => p.id === id) || null;
-  }, [productList]);
-
-  const selectProduct = useCallback((id: number) => {
-    if (formActive && formDirty) {
-      return;
-    }
-    setSelectedProduct(getProductById(id));
-    setFormActive(false);
-  }, [formActive, formDirty, getProductById]);
-
-  const startProductCreate = useCallback(() => {
-    setSelectedProduct(null);
-    startProductEdit();
-  }, []);
-  const startProductEdit = useCallback((id?: number) => {
-    id && selectProduct(id);
-    setFormActive(true);
-    setFormDirty(false);
-  }, [selectProduct]);
-  
   const concludeProductEdit = useCallback((product: Omit<ProductType, 'id'>, id?: number) => {
-    setFormActive(false);
+    const productIndex = id ? productList.findIndex(p => p.id === id) : productList.length;
+    id ??= Math.max(...productList.map(p => p.id)) + 1;
     const newProductList = productList.slice();
-    if (!id) {
-      const newProductId = Math.max(...newProductList.map(p => p.id)) + 1;
-      newProductList.push({ ...product, id: newProductId });
-      setProductList(newProductList);
-      selectProduct(newProductId);
-      return;
-    }
-    const productIndex = newProductList.findIndex(p => p.id === id);
     newProductList[productIndex] = { ...product, id };
     setProductList(newProductList);
-    selectProduct(id);
   }, [productList]);
 
   const deleteProduct = useCallback((id: number) => {
@@ -63,22 +36,33 @@ export default function Shop({ products }: { products: Array<ProductType> }) {
     <div className="shop">
       <ProductList
         products={productList}
-        onSelect={selectProduct}
-        onNew={startProductCreate}
-        onEdit={startProductEdit}
+        onSelect={id => {
+          setSelectedProductId(id);
+          setFormActive(false);
+        }}
+        onCreate={() => {
+          setSelectedProductId(null);
+          setFormActive(true);
+          setFormDirty(false);
+        }}
+        onEdit={id => {
+          setSelectedProductId(id);
+          setFormActive(true);
+          setFormDirty(false);
+        }}
         onDelete={deleteProduct}
         buttonsDisabled={formActive && formDirty}
-        forcedSelectedProductId={(formActive && formDirty) ? selectedProduct?.id : undefined}
+        selectionChangeDisabled={formActive && formDirty}
       />
       {formActive && (
-        // не уверен, что это юзкейс key. руководствовался тем, что если выбрать другой продукт, пока открыта форма,
-        // то formActive не поменяется, весь этот кусок не перерисуется, и форма останется старой. поэтому заставляю его
-        // по-любому перерисовываться, если выбран другой продукт
         <ProductEditForm
           key={selectedProduct?.id ?? 0}
           product={selectedProduct || undefined}
           onDirty={() => setFormDirty(true)}
-          onSubmit={formValues => concludeProductEdit(formValues as ProductType, selectedProduct?.id)}
+          onSubmit={formValues => {
+            concludeProductEdit(formValues as ProductType, selectedProduct?.id);
+            setFormActive(false);
+          }}
           onCancel={() => { setFormActive(false); setFormDirty(false); }}
         />
       )}
