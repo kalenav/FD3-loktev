6a801f8f095c000868f83639a8bc9815c9475d82
Kanstantsin Loktsieu <konstantin.loktev@innowise.com>
2026-03-18 10:08:05 +0100

    refactor

--- rainbowframe/src/RainbowFrame.tsx
+++ rainbowframe/src/RainbowFrame.tsx
@@ -1,12 +1,5 @@
-import { JSX, useEffect, useState } from "react";
+import { JSX } from "react";
 
 export function RainbowFrame({ colors, children }: { colors: string[], children: JSX.Element }) {
-  const [content, setContent] = useState<JSX.Element>(children);
-  useEffect(() => {
-    setContent(children);
-    colors.forEach(color => setContent(prevContent => (
-      <div style={{ border: `solid ${color} 2px` }}>{prevContent}</div>
-    )));
-  }, [colors, children]);
-  return content;
+  return colors.reduce((content, color) => <div style={{ border: `solid ${color} 2px` }}>{content}</div>, children);
 }
