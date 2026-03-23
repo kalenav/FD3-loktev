1ac2a26811b6234afc15d33f352702ff0246c485
Kanstantsin Loktsieu <konstantin.loktev@innowise.com>
2026-03-23 09:30:27 +0100

    set up finnhub websocket context

---
+++ graduation/src/contexts/finnhub-ws.context.tsx
@@ -0,0 +1,69 @@
+import { createContext, useCallback, useContext, useEffect, useMemo, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { addStockDataPoint } from "../redux/stock-data.slice";
+import type { FinnhubTradeMessage } from "../types/finnhub-trade-message.interface";
+
+const FinnhubWebSocketContext = createContext<{
+  subscribeToSymbolUpdates: (symbol: string) => void,
+}>({
+  subscribeToSymbolUpdates: () => {},
+});
+
+export function FinnhubWebSocketProvider({ children }: { children: React.ReactNode }) {
+  const ws = useMemo(() => new WebSocket(`wss://ws.finnhub.io?token=${import.meta.env.VITE_FINNHUB_API_KEY}`), []);
+  const symbolUpdatesQueue = useRef<string[]>([]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const onOpen = () => {
+      symbolUpdatesQueue.current.forEach(subscribeToSymbolUpdates);
+      symbolUpdatesQueue.current.length = 0;
+    };
+    const onMessage = (rawMessage: { data: string }) => {
+      const message = JSON.parse(rawMessage.data);
+      switch (message.type) {
+        case 'ping':
+          ws.send(JSON.stringify({ type: 'pong' }));
+          break;
+        case 'trade':
+          (message as FinnhubTradeMessage).data.forEach(trade => {
+            dispatch(addStockDataPoint({
+              stockSymbol: trade.s,
+              timestamp: trade.t,
+              price: trade.p,
+            }));
+          });
+          break;
+        default:
+          break;
+      }
+    };
+    ws.addEventListener('open', onOpen);
+    ws.addEventListener('message', onMessage);
+    return () => {
+      ws.removeEventListener('open', onOpen);
+      ws.removeEventListener('message', onMessage);
+      ws.close();
+    }
+  }, [ws]);
+
+  const subscribeToSymbolUpdates = useCallback((symbol: string) => {
+    if (ws.readyState === WebSocket.CONNECTING) {
+      symbolUpdatesQueue.current.push(symbol);
+      return;
+    }
+    if (ws.readyState !== WebSocket.OPEN) {
+      console.warn('websocket already closing or closed :(');
+      return;
+    }
+    ws.send(JSON.stringify({ type: "subscribe", symbol }));
+  }, [ws]);
+
+  return (
+    <FinnhubWebSocketContext.Provider value={{ subscribeToSymbolUpdates }}>
+      {children}
+    </FinnhubWebSocketContext.Provider>
+  );
+}
+
+export const useFinnhubWS = () => useContext(FinnhubWebSocketContext);
