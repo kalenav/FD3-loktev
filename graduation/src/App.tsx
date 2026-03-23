b0e25ed9dacee8ea037ec8aec80b3e84c0fb75c7
Kanstantsin Loktsieu <konstantin.loktev@innowise.com>
2026-03-23 12:30:29 +0100

    stock chart component wireframe

--- graduation/src/App.tsx
+++ graduation/src/App.tsx
@@ -1,10 +1,11 @@
+import { StockChart } from "./components/StockChart/StockChart";
 import { useLiveStockData } from "./hooks/use-live-stock-data";
 
 function App() {
   const stockData = useLiveStockData(['BINANCE:BTCUSDT']);
-  console.log(stockData);
+
   return (
-    <></>
+    <StockChart data={stockData['BINANCE:BTCUSDT'] || []} />
   )
 }
 
