a13c2cde7e23131fbee16b6015fcb2453ff9ddeb
Kanstantsin Loktsieu <konstantin.loktev@innowise.com>
2026-03-23 09:31:18 +0100

    successfully receiving messages from finnhub ws

--- graduation/src/App.tsx
+++ graduation/src/App.tsx
@@ -1,4 +1,8 @@
+import { useLiveStockData } from "./hooks/use-live-stock-data";
+
 function App() {
+  const stockData = useLiveStockData(['BINANCE:BTCUSDT']);
+  console.log(stockData);
   return (
     <></>
   )
--- graduation/src/main.tsx
+++ graduation/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import App from './App.tsx'
+import { FinnhubWebSocketProvider } from './contexts/finnhub-ws.context.tsx'
 import './index.scss'
+import { store } from './redux/store.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+    <Provider store={store}>
+      <FinnhubWebSocketProvider>
+        <App />
+      </FinnhubWebSocketProvider>
+    </Provider>
+  // </StrictMode>,
 )
