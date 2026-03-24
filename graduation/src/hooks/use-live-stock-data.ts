6dbaf44da89e9e5a929fc41782da45c592d95235
Kanstantsin Loktsieu <konstantin.loktev@innowise.com>
2026-03-24 09:36:57 +0100

    adjust use live stock data hook

--- graduation/src/hooks/use-live-stock-data.ts
+++ graduation/src/hooks/use-live-stock-data.ts
@@ -6,9 +6,10 @@ import type { StockDataState } from "../redux/stock-data.slice";
 export function useLiveStockData(symbols: string[]) {
   const { subscribeToSymbolUpdates } = useFinnhubWS();
   const stockData = useSelector((state: { stockData: StockDataState }) => state.stockData);
+  const selectedSymbols = useSelector((state: { selectedSymbols: Array<string> }) => state.selectedSymbols);
   useEffect(() => {
-    symbols.filter(symbol => !(symbol in stockData)).forEach(subscribeToSymbolUpdates);
-  }, [subscribeToSymbolUpdates, stockData]);
+    symbols.filter(symbol => !selectedSymbols.includes(symbol)).forEach(subscribeToSymbolUpdates);
+  }, [subscribeToSymbolUpdates, selectedSymbols]);
 
   return Object.fromEntries(symbols.map(symbol => [symbol, stockData[symbol] ?? []]));
 }
