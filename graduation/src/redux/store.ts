54f2aa94e0a5f92137ef7745ca19032b93bcf2b6
Kanstantsin Loktsieu <konstantin.loktev@innowise.com>
2026-03-24 09:36:52 +0100

    add selected symbols slice to store

--- graduation/src/redux/store.ts
+++ graduation/src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import selectedSymbolsReducer from "./selected-symbols.slice";
 import { stockDataThrottlerMiddleware } from "./stock-data-throttler.middleware";
 import stockDataSliceReducer from "./stock-data.slice";
 
 export const store = configureStore({
   reducer: {
-    stockData: stockDataSliceReducer
+    stockData: stockDataSliceReducer,
+    selectedSymbols: selectedSymbolsReducer,
   },
   middleware: (getDefault) => getDefault().concat(stockDataThrottlerMiddleware)
 });
